29502cecc6e15b59a694483db8446ac3cd70409a
SAFFFFFFF7 <saf.alg699@gmail.com>
2025-03-26 11:28:47 +0100

    zdenjakofcar

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php
-$pdo = new PDO("mysql:host=http://172.16.19.129;dbname=injection", "root", "secret", [
+$pdo = new PDO("mysql:host=172.16.19.129;dbname=injection", "root", "secret", [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
 ]);
 
